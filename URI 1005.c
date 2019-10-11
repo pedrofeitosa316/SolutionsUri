2c34dcfdacf4bdb30bf0afd41baa2d2a83de59eb
Junior <36310159+souzajunior@users.noreply.github.com>
2019-10-11 10:57:13 -0300

    Formatação de código e visualização

--- URI 1005.c
+++ URI 1005.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
  
 int main() {
- 
-    float a, b, media;
+	float a, b, media;
 	scanf("%f", &a);
 	scanf("%f", &b);
-	media = (a * 3.5 + b * 7.5) / 11;
+	
+	media = ((a * 3.5 + b * 7.5) / 11);
 	printf("MEDIA = %.5f\n", media); 
  
     return 0;
